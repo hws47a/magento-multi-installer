9083ab7884cfdee40c16c4f63ab3c17cbd07ee9f
Vladimir Fishchenko <hws47a@gmail.com>
2014-04-30 12:30:43 +0400

    Added checking exist for config.php and builds.txt

--- run.php
+++ run.php
@@ -1,7 +1,16 @@
 <?php
 define('BASE_PATH', __DIR__ . DIRECTORY_SEPARATOR);
-require BASE_PATH . 'config.php';
 require BASE_PATH . 'include/loader.php';
+
+//check for config exist
+if (!file_exists(BASE_PATH . 'config.php')) {
+    \Core\fatal('Please, create config.php file, you can use config.php.example to see how it should look.');
+}
+if (!file_exists(BASE_PATH . 'builds.txt')) {
+    \Core\fatal('Please, create builds.txt file, you can use builds.txt.example to see how it should look.');
+}
+
+require BASE_PATH . 'config.php';
 
 //check path to install and create directory
 
